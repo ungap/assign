bfd34e3eedf38c0ee49ce49f9a3d0fc80b8f5651
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-05-02 14:52:38 +0200

    made it dualmodule

--- min.js
+++ min.js
@@ -1,2 +1,2 @@
 /*! (c) Andrea Giammarchi - ISC */
-var assign=Object.assign||function(r){for(var n,a=1;a<arguments.length;a++){n=arguments[a]||{};for(var s in n)n.hasOwnProperty(s)&&(r[s]=n[s])}return r};
+var assign=Object.assign||function(r){for(var n,a=1;a<arguments.length;a++)for(var s in n=arguments[a]||{})n.hasOwnProperty(s)&&(r[s]=n[s]);return r};
